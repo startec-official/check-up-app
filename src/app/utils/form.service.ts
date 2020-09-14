7c266d2810b56098b0a89d073b22115e8d8703d2
startec-official <startec.innovations@gmail.com>
2020-09-14 10:59:58 +0800

    create form service

---
+++ src/app/utils/form.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  constructor() { }
+
+  generatePersonalForm() {
+    return new FormGroup({
+      'lastName' : new FormControl('',Validators.required),
+      'firstName' : new FormControl('',Validators.required),
+      'MI' : new FormControl('',[Validators.required,Validators.maxLength(1)]),
+      'specialty' : new FormControl('',Validators.required)
+    });
+  }
+
+  generateNewSchedEntryForm( day? : string ,
+                             timeindef? : string ,
+                             timeoutdef? : string ,
+                             appCountdef? : number ) : FormArray {
+    return new FormArray([
+      new FormGroup({
+        appDate : new FormControl( ( day || '' ) ),
+        timein : new FormControl( (timeindef || '') ,Validators.required),
+        timeout : new FormControl( (timeoutdef || '') ,Validators.required),
+        appCount : new FormControl( (appCountdef || '') ,Validators.required)
+       })
+    ])
+  }
+}
--- src/app/week-schedule/week-schedule.component.ts
+++ src/app/week-schedule/week-schedule.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { UtilsService } from '../utils/utils.service';
-import { FormArray, FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+import { FormArray, FormGroup } from '@angular/forms';
 import { AppEntry } from '../utils/app-entry';
 import { HttpClient } from '@angular/common/http';
 import { HttpAppService } from '../utils/http.app.service';
+import { FormService } from '../utils/form.service';
 
 @Component({
   selector: 'app-week-schedule',
@@ -24,41 +25,26 @@ export class WeekScheduleComponent implements OnInit {
 
   private utilsService : UtilsService;
   
-  constructor( _utilsService : UtilsService , private httpService : HttpAppService ) {
+  constructor( _utilsService : UtilsService ,
+               private httpService : HttpAppService ,
+               private formService : FormService ) {
     this.utilsService = _utilsService;
   }
 
   ngOnInit(): void { // TODO: place form processes into service
     this.daysOfWeek = this.utilsService.getCurrentWeek();
 
-    this.personalInfo = new FormGroup({
-      'lastName' : new FormControl('',Validators.required),
-      'firstName' : new FormControl('',Validators.required),
-      'MI' : new FormControl('',[Validators.required,Validators.maxLength(1)]),
-      'specialty' : new FormControl('',Validators.required)
-    });
+    this.personalInfo = this.formService.generatePersonalForm();
 
     let i = 0;
     this.daysOfWeek.forEach(day => {
       this.dayFormDisabled[i ++] = false;
-      this.dayForms.push( new FormArray([
-        new FormGroup({
-          appDate : new FormControl( day ),
-          timein : new FormControl('',Validators.required),
-          timeout : new FormControl('',Validators.required),
-          appCount : new FormControl('',Validators.required)
-         })
-      ]));
+      this.dayForms.push( this.formService.generateNewSchedEntryForm( day ) );
     });
   }
 
   addGroup( dayForm : FormArray , inputAppDate : string ) {
-    dayForm.push( new FormGroup({
-      appDate : new FormControl( inputAppDate ),
-      timein : new FormControl('',Validators.required),
-      timeout : new FormControl('',Validators.required),
-      appCount : new FormControl('',Validators.required)
-    }) );
+    dayForm.push( this.formService.generateNewSchedEntryForm(inputAppDate) );
   }
 
   removeBlock( dayForm : FormArray, index : number ) {
